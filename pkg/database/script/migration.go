72263bd77836f4f98d0ad81d7e9969dcd1ee4c96
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-03-24 05:01:30 +0700

    add auth migration

--- pkg/database/script/migration.go
+++ pkg/database/script/migration.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+	"tansan/config"
+	"tansan/pkg/database/migration"
+)
+
 func main() {
-	// This is a comment
+	ctx := context.Background()
+	_ = ctx
+
+	// Initialize configuration
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Please provide the path to the .env file")
+		}
+		return os.Args[1]
+	}())
+
+	switch cfg.App.Name {
+	case "user":
+	case "auth":
+		migration.AuthMigrate(ctx, &cfg)
+	case "book":
+	case "userbook":
+	case "payment":
+	}
 }
