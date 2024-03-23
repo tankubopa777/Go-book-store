90e4c289e0d3fffff8bccf770f47dd40ce33dd9b
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-03-23 23:34:06 +0700

    update user pb

--- modules/user/userHandler/userGrpcHandler.go
+++ modules/user/userHandler/userGrpcHandler.go
@@ -1,15 +1,32 @@
 package userHandler
 
 import (
+	"context"
+	userPb "tansan/modules/user/userPb"
 	"tansan/modules/user/userUsecase"
 )
 
 type (
-	userGrpcHandlerService struct {
+	userGrpcHandler struct {
 		userUsecase	userUsecase.UserUsecaseService
+		userPb.UnimplementedUserGrpcServiceServer
 	}
 )
 
-func NewUserGrpcHandler(userUsecase	userUsecase.UserUsecaseService) *userGrpcHandlerService {
-	return &userGrpcHandlerService{userUsecase}
+func NewUserGrpcHandler(userUsecase	userUsecase.UserUsecaseService) *userGrpcHandler {
+	return &userGrpcHandler{
+		userUsecase: userUsecase,
+	}
+}
+
+func (g *userGrpcHandler) CredentialSearch(ctx context.Context, req *userPb.CredentialSearchReq) (*userPb.UserProfile, error) {
+	return nil, nil
+}
+
+func (g *userGrpcHandler) FindOneUserProfileToRefresh(ctx context.Context, req *userPb.FindOneUserProfileToRefreshReq) (*userPb.UserProfile, error) {
+	return nil, nil
+}
+
+func (g *userGrpcHandler) GetPlayerSavingAccount(ctx context.Context, req *userPb.GetUserSavingAccountReq) (*userPb.GetUserSavingAccountRes, error) {
+	return nil, nil
 }
